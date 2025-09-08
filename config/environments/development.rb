1b178b39a8a0622d4d61321c4ca0dcb765f3f92b
Karen Barbé <info@karenbarbe.com>
2025-09-08 10:47:49 -0500

    install devise gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -83,4 +83,8 @@ Rails.application.configure do
 
   # Allow POST authenticity on Codespaces in dev
   config.action_controller.forgery_protection_origin_check = false
+
+  # Define default url for dev environment
+  # In production, :host should be set to the actual host of your application
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 end
